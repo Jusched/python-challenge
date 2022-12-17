bcee52b7b77841abab2fe9c88cadfd9112a605ef
Jusched <juans-betancurt@unilibre.edu.co>
2022-12-16 21:41:03 -0500

    challenge finished.

--- ada/challenge/models/booking.py
+++ ada/challenge/models/booking.py
@@ -7,10 +7,20 @@ class Booking(models.Model):
     email= models.EmailField(max_length=30)
     origin= models.CharField(max_length=15)
     destination= models.CharField(max_length=15)
+    duration= models.CharField(max_length=8)
     departure_date= models.DateField()
     time_departure= models.TimeField()
-    duration= models.TimeField()
+    
 
 
     def __str__(self):
         return f'Booking made by {self.name}, on {self.origin}, in order to go to {self.destination} on {self.departure_date} at {self.time_departure} for {self.duration}'
+
+# DateField
+# YYYY-MM-DD
+
+# TimeField
+# '14:30:59'
+# '14:30'
+
+# '# days'
